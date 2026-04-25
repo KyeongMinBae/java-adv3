4ce845e2a25bc701854d1cd77a5954d54940611f
KyeongMinBae <reaaaaaaal@gmail.com>
2026-04-25 22:40:29 +0900

    7. 람다가 필요한 이유3

---
+++ src/ramda/start/Ex2RefMain.java
@@ -0,0 +1,45 @@
+package ramda.start;
+
+import ramda.Procedure;
+
+import java.util.Random;
+
+// 익명 클래스 사용
+public class Ex2RefMain {
+
+    public static void hello(Procedure procedure) {
+        long startNs = System.nanoTime();
+
+        //코드 조각 시작
+        procedure.run();
+        //코드 조각 종료
+
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간: " + (endNs - startNs) + "ns");
+    }
+
+    public static void main(String[] args) {
+        Procedure dice = new Procedure() {
+            @Override
+            public void run() {
+
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("i = " + i);
+                }
+
+            }
+        };
+
+        Procedure sum = new Procedure() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("i = " + i);
+                }
+            }
+        };
+
+        hello(dice);
+        hello(sum);
+    }
+}
---
+++ src/ramda/start/Ex4RefMain.java
@@ -0,0 +1,32 @@
+package ramda.start;
+
+import ramda.Procedure;
+
+// 익명 클래스 사용, 변수 제거, 익명 클래스의 참조값을 매개변수(파라미터)에 직접 전달
+public class Ex4RefMain {
+
+    public static void hello(Procedure procedure) {
+        long startNs = System.nanoTime();
+
+        //코드 조각 시작
+        procedure.run();
+        //코드 조각 종료
+
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간: " + (endNs - startNs) + "ns");
+    }
+
+    public static void main(String[] args) {
+        hello(() -> {
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("i = " + i);
+            }
+        });
+
+        hello(() -> {
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("i = " + i);
+            }
+        });
+    }
+}
