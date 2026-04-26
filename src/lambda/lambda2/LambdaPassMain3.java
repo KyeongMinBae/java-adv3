e9348b2c02670ca6d19c7da3c9850270c5272de3
KyeongMinBae <reaaaaaaal@gmail.com>
2026-04-26 18:28:15 +0900

    14. 람다의 전달

---
+++ src/lambda/lambda2/LambdaPassMain3.java
@@ -0,0 +1,32 @@
+package lambda.lambda2;
+
+import lambda.MyFunction;
+
+// 2. 람다를 메서드(함수)에 전달하기
+public class LambdaPassMain3 {
+
+    public static void main(String[] args) {
+        MyFunction add = getOperation("add");
+        System.out.println("add.apply(1, 2) = " + add.apply(1, 2));
+
+        MyFunction sub = getOperation("sub");
+        System.out.println("sub.apply(1, 2) = " + sub.apply(1, 2));
+
+        MyFunction xxx = getOperation("xxx");
+        System.out.println("xxx.apply(1, 2) = " + xxx.apply(1, 2));
+
+    }
+
+    // 람다를 반환하는 메서드
+    static MyFunction getOperation(String operator) {
+        switch (operator) {
+            case "add":
+                return (a, b) -> a + b;
+            case "sub":
+                return (a, b) -> a- b;
+            default:
+                return  (a, b) -> 0;
+        }
+    }
+
+}
